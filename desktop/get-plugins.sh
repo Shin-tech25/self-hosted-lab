0924edf4eecc01f107c6c99084a552f696d86f3e
Shin Mikami <m.shin0509@gmail.com>
2025-03-25 06:54:17 +0000

    fix(get-plugins.sh): プラグイン追加スクリプトはコンテナに同梱されているため不要

--- desktop/get-plugins.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ! -d init ]; then
-    mkdir -p init
-fi
-
-curl -L https://downloads.apache.org/guacamole/1.5.3/binary/guacamole-auth-jdbc-1.5.3.tar.gz \
-  | tar -xz --strip-components=3 -C init guacamole-auth-jdbc-1.5.3/mysql/schema
