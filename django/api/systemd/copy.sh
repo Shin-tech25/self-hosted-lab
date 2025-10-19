6e55d122d4a907aa21ec27d8875c6d93e56b9867
Shin Mikami <m.shin0509@gmail.com>
2025-10-19 10:59:33 +0900

    Remove copy.sh script

--- django/api/systemd/copy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo cp ./gunicorn.service /etc/systemd/system/gunicorn.service
-sudo systemctl daemon-reload
-sudo systemctl enable gunicorn.service
-sudo systemctl restart gunicorn.service
